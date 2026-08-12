0ac03356c6784364d0de8b33672798acc7ea793a
Roshan Sinhabhu <roshansinhabahu@gmail.com>
2026-08-12 12:40:48 +0530

    feat: add functionality to retrieve divisional secretariats by province and enhance error handling

--- src/controllers/divisional-secretariat.controller.js
+++ src/controllers/divisional-secretariat.controller.js
@@ -1,4 +1,4 @@
-import { getAllDivisionalSecretariats,findDivisionalSecretariatsByDistrict } from "../services/divisional-secretariat.service.js";
+import { getAllDivisionalSecretariats,findDivisionalSecretariatsByDistrict, findDivisionalSecretariatsByProvince } from "../services/divisional-secretariat.service.js";
 import { PROVINCE_SLUGS,DISTRICT_SLUGS } from "../constants/provinces.js";
 
 import { successResponse } from "../utils/response.js";
@@ -6,10 +6,12 @@ import { successResponse } from "../utils/response.js";
 export async function getDivisionalSecretariats(req, res, next) {
     try {
         const { district } = req.query;
+        const { province } = req.query;
 
         let divisionalSecretariats;
         let data;
 
+        //error if empty
         if (district !== undefined && district.trim() === "") {
 
              const error = new Error("District cannot be empty");
@@ -18,13 +20,33 @@ export async function getDivisionalSecretariats(req, res, next) {
              return next(error);
         }
 
-        const normalizedDistricts = district?.toLowerCase();
+        if (province !== undefined && province.trim() === "") {
 
+             const error = new Error("Province cannot be empty");
+             error.statusCode = 400;
+
+             return next(error);
+        }
+
+        const normalizedDistricts = district?.toLowerCase();
+        const normalizedProvince = province?.toLowerCase();
+
+        //error if wrong
         if (
             normalizedDistricts &&
             !DISTRICT_SLUGS.includes(normalizedDistricts)
         ) {
             const error = new Error("Invalid district, Check for spellings or documentation!");
+            error.statusCode = 400;
+
+            return next(error);
+        }
+
+        if (
+            normalizedProvince &&
+            !PROVINCE_SLUGS.includes(normalizedProvince)
+        ) {
+            const error = new Error("Invalid Province, Check for spellings or documentation!");
             error.statusCode = 400;
 
             return next(error);
@@ -49,7 +71,28 @@ export async function getDivisionalSecretariats(req, res, next) {
                         code: ds.province_code
                     }
                 }));
-        } else {
+        } else if(normalizedProvince){
+            divisionalSecretariats = await findDivisionalSecretariatsByProvince(normalizedProvince);
+                data = divisionalSecretariats.map((ds) => ({
+                    id: ds.id,
+                    name: ds.name,
+                    slug: ds.slug,
+
+                    district: {
+                        id: ds.district_id,
+                        name: ds.district_name,
+                        code: ds.district_code
+                    },
+
+                    province: {
+                        id: ds.province_id,
+                        name: ds.province_name,
+                        code: ds.province_code
+                    }
+                }));
+        }
+
+        else {
             divisionalSecretariats = await getAllDivisionalSecretariats();
                 data = divisionalSecretariats.map((ds) => ({
                     id: ds.id,
--- src/services/divisional-secretariat.service.js
+++ src/services/divisional-secretariat.service.js
@@ -44,3 +44,30 @@ export async function findDivisionalSecretariatsByDistrict(districtSlug) {
 
     return result.rows;
 }
+
+export async function findDivisionalSecretariatsByProvince(provinceSlug) {
+    const result = await db.query(
+        `
+        SELECT
+            ds.id,
+            ds.name,
+            ds.slug,
+            d.id AS district_id,
+            d.name AS district_name,
+            d.code AS district_code,
+            p.name AS province_name,
+            p.code AS province_code,
+            p.id AS province_id
+        FROM divisional_secretariats ds
+        JOIN districts d
+            ON ds.district_id = d.id
+        JOIN provinces p
+            ON d.province_id = p.id
+        WHERE p.slug = $1
+        ORDER BY ds.id
+        `,
+        [provinceSlug]
+    );
+
+    return result.rows;
+}
